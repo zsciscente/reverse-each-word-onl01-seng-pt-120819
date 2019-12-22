33ae12eab5b10b8adefe1e7a530465784a7a74f6
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 03:48:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 
-def reverse_each_word(arguement)
-  new_array = []
-  new_array << arguement.split
-new_array.collect {|word| 
-"#{word}.reverse" }
-end
  
   
+
+def reverse_each_word(sentence1)
+  new_array = []
+  new_array << sentence1.split.collect(&:reverse!).join(' ')
+ new_array
+end
